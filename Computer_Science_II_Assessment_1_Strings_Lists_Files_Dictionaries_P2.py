b2e28e4ab63f1db2ba1c8ad93b849568e7fd84ff
AsterixAndObelix <31694091+AsterixAndObelix@users.noreply.github.com>
2017-09-29 13:56:02 -0700

    Add files via upload

---
+++ Computer_Science_II_Assessment_1_Strings_Lists_Files_Dictionaries_P2.py
@@ -0,0 +1,28 @@
+import random
+
+numbers = []
+addition = 0
+happy = False
+
+while happy == False:
+    for counter in range(100):
+        num = random.randint(0 , 1000)
+        numbers.append(num)
+        addition += counter
+
+    average = addition / 100
+
+    print(numbers)
+    print(average)
+
+    check = input("Are you happy?")
+
+    def reoccur(check, message):
+        if check == "Yes":
+            happy = True
+        elif check == "No":
+            happy = False
+        else:
+            raise incorrect
+
+    reoccur(check)
